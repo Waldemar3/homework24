3cd05ec320f9838187336bf6f09951449371e14e
Waldemar3 <68418282+Waldemar3@users.noreply.github.com>
2020-10-05 13:00:09 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,72 +18,74 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return auth()->user();
 });
 
-Route::get('/users', function (){
-    return new \App\Http\Resources\UserCollection(\App\User::all());
-});
+Route::middleware(['auth'])->group(function() {
+    Route::get('/users', function () {
+        return new \App\Http\Resources\UserCollection(\App\User::all());
+    });
 
-Route::get('/users/{user}', function ($user){
-    return new \App\Http\Resources\User(\App\User::find($user));
-});
+    Route::get('/users/{user}', function ($user) {
+        return new \App\Http\Resources\User(\App\User::find($user));
+    });
 
-Route::post('/users', function (\Illuminate\Http\Request $request){
-    $validate = $request->validate([
-        'username' => 'required|min:3|max:16',
-        'email' => 'required|email',
-        'password' => 'min:6|max:50',
-    ]);
+    Route::post('/users', function (\Illuminate\Http\Request $request) {
+        $validate = $request->validate([
+            'username' => 'required|min:3|max:16',
+            'email' => 'required|email',
+            'password' => 'min:6|max:50',
+        ]);
 
-    $username = $request->get('username');
-    $email = $request->get('email');
-    $password = $request->get('password');
+        $username = $request->get('username');
+        $email = $request->get('email');
+        $password = $request->get('password');
 
-    $user = new \App\User();
-    $user->name = $username;
-    $user->email = $email;
-    $user->email_verified_at = now();
-    $user->password = \Illuminate\Support\Facades\Hash::make($password);
-    $user->remember_token = \Illuminate\Support\Str::random(10);
-    $user->save();
+        $user = new \App\User();
+        $user->name = $username;
+        $user->email = $email;
+        $user->email_verified_at = now();
+        $user->password = \Illuminate\Support\Facades\Hash::make($password);
+        $user->remember_token = \Illuminate\Support\Str::random(10);
+        $user->save();
 
-    return new \App\Http\Resources\User($user);
-});
+        return new \App\Http\Resources\User($user);
+    });
 
-Route::put('/users/{user}', function (\App\User $user, \Illuminate\Http\Request $request){
-    $validate = $request->validate([
-        'username' => 'required|min:3|max:16',
-        'email' => 'required|email',
-        'password' => 'min:6|max:50',
-    ]);
+    Route::put('/users/{user}', function (\App\User $user, \Illuminate\Http\Request $request) {
+        $validate = $request->validate([
+            'username' => 'required|min:3|max:16',
+            'email' => 'required|email',
+            'password' => 'min:6|max:50',
+        ]);
 
-    $username = $request->get('username');
-    $email = $request->get('email');
-    $password = $request->get('password');
+        $username = $request->get('username');
+        $email = $request->get('email');
+        $password = $request->get('password');
 
-    $user->name = $username;
-    $user->email = $email;
-    $user->password = \Illuminate\Support\Facades\Hash::make($password);
-    $user->save();
+        $user->name = $username;
+        $user->email = $email;
+        $user->password = \Illuminate\Support\Facades\Hash::make($password);
+        $user->save();
 
-    return new \App\Http\Resources\User($user);
-});
+        return new \App\Http\Resources\User($user);
+    });
 
-Route::patch('/users/{user}', function (\App\User $user, \Illuminate\Http\Request $request){
-    $validate = $request->validate([
-        'password' => 'min:6|max:50',
-    ]);
+    Route::patch('/users/{user}', function (\App\User $user, \Illuminate\Http\Request $request) {
+        $validate = $request->validate([
+            'password' => 'min:6|max:50',
+        ]);
 
-    $password = $request->get('password');
+        $password = $request->get('password');
 
-    $user->password = \Illuminate\Support\Facades\Hash::make($password);
-    $user->save();
+        $user->password = \Illuminate\Support\Facades\Hash::make($password);
+        $user->save();
 
-    return response([$user->name => "Password changed"]);
-});
+        return response([$user->name => "Password changed"]);
+    });
 
-Route::delete('/users/{user}', function (\App\User $user){
-    $user->delete();
+    Route::delete('/users/{user}', function (\App\User $user) {
+        $user->delete();
 
-    return response([$user->name => 'deleted']);
+        return response([$user->name => 'deleted']);
+    });
 });
 
 Route::post('/auth/sign-up', function (\Illuminate\Http\Request $request){
